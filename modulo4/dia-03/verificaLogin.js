c966be4858b6b91de5bc2d6b055c6a1f17760683
Thiago Vernizzi Silva <thiago.vernizzi@gmail.com>
2022-06-23 08:09:46 -0300

    atualizando arquivos

--- modulo4/dia-03/verificaLogin.js
+++ modulo4/dia-03/verificaLogin.js
@@ -4,47 +4,45 @@
 3- Dentro da função, iteramos a lista de cadastro
 3.1- a cada iteração, verificamos se o e-mail recebido via argumento é igual ao da iteração
 3.1.1- se os e-mails forem iguais, verificamos se as senhas também são
-3.1.1.1- se as senhas forem iguais, retornamos a frase "login bem-sucedido"
+3.1.1.1- se as senhas forem iguais, retoramos a frase "login bem-sucedido"
 4- Dentro da função, retornamos a frase "e-mail ou senha inválidos"
 */
 
 const contas = [
-	{
-	  email: "astrodev@labenu.com",
-	  password: "abc123",
-	},
-	{
-	  email: "bananinha@gmail.com",
-	  password: "bananinha",
-	},
-  ];
-  
-  const validaLogin = (email, password) => {
-	if (email.includes("@")) {
-	  if (password.length >= 6) {
-		
-		let checaEmail = contas.findIndex(conta => conta.email === email)
-		let checaPassword = contas.findIndex(conta => conta.password === password)
-  
-		if (checaEmail === checaPassword) {
-		  return "Login realizado com sucesso!"
-		} else {
-		  return "Email ou senha incorretos."
-		}
-  
-	  } else {
-		return "Senha deve possuir no mínimo 6 caracteres"
-	  }
-	  
-	} else {
-	  return "Email inválido"
-	}
+  {
+    email: "astrodev@labenu.com",
+    senha: "abc123",
+  },
+  {
+    email: "bananinha@gmail.com",
+    senha: "bananinha",
+  },
+];
+
+const verificaLogin = (email, senha) => {
+  if (email.includes("@")) {
+    if (senha.length >= 6) {
+      
+      let validaEmail = contas.findIndex(conta => conta.email === email);
+      let validaSenha = contas.findIndex(conta => conta.senha === senha);
+
+      if (validaEmail === validaSenha) {
+        return console.log("Login realizado com sucesso!");
+      } else {
+        return console.log("Email ou senha incorretos.");
+      }
+
+    } else {
+      return console.log("Senha deve possuir no mínimo 6 caracteres");
+    }
+    
+  } else {
+    return console.log("Email inválido");
   }
-  
-  console.log(validaLogin("astrodev@labenu.com", "abc123"))
-  console.log(validaLogin("bananinha@gmail.com", "bananinha"))
-  console.log(validaLogin("astrodev.labenu.com", "abc123"))
-  console.log(validaLogin("astrodev@labenu.com", "abc12"))
-  console.log(validaLogin("bananinha@gmail.com", "banana"))
-  console.log(validaLogin("bananinha@gmail.com", "banan"))
-  console.log(validaLogin("pamela@gmail.com", "123456"))
+}
+
+verificaLogin("astrodev.labenu.com", "abc123");
+verificaLogin("astrodev@labenu.com", "abc123"); 
+verificaLogin("astrodev@labenu.com", "abc1");
+verificaLogin("bananinha@gmail.com", "banana");
+verificaLogin("bananinha@gmail.com", "bananinha");
