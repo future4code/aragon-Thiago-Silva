a692384beaad1bbe0c484441752f03b876c27af6
Thiago Vernizzi Silva <thiago.vernizzi@gmail.com>
2022-07-26 20:11:30 -0300

    refatorando labecommerce - exercício herança

---
+++ modulo5/heranca/src/endpoints/getUsers.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express"
+import { UserDatabase } from "../database/UserDataBase"
+
+export const getUsers = async (req: Request, res: Response) => {
+    let errorCode = 400
+    try {
+        // const result = await connection(TABLE_USERS).select()
+        const userDatabase = new UserDatabase()
+        const result = await userDatabase.getAllUsers()
+
+        res.status(200).send({ users: result })
+    } catch (error) {
+        res.status(errorCode).send({ message: error.message })
+    }
+}
